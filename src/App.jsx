00c1d32c5fa24c8c361926af67c7ad16dbb0033e
thara-1234 <tharakrishna1989@gmail.com>
2025-04-26 17:12:55 +0530

    again committed branch thara1 with develop1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -21,13 +21,21 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button  onClick={() => alert(`clicked button 2`)} 
-        style={{ backgroundColor: 'violet', color: 'white', padding: '10px', border: 'none', borderRadius: '5px' }}
->Click Button 2</button>
-        <button onClick={()=>alert('Hi shahana')}>Shahana</button>
-        </button><br/>
-        <button onClick={()=>{alert('I am thara1')}}>Show</button>
-
+        <button
+          onClick={() => alert(`clicked button 2`)}
+          style={{
+            backgroundColor: "violet",
+            color: "white",
+            padding: "10px",
+            border: "none",
+            borderRadius: "5px",
+          }}
+        >
+          Click Button 2
+        </button>
+        <button onClick={() => alert("Hi shahana")}>Shahana</button>
+        <br />
+        <button onClick={() => {alert("I am thara1")}}>Show</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -36,7 +44,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
