3b188bcc2e9f60da3749f7492c4404a8d8013c13
thara-1234 <tharakrishna1989@gmail.com>
2025-04-26 16:56:54 +0530

    initial commit branch thara1

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,9 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button><br/>
+        <button onClick={()=>{alert('I am thara1')}}>Show</button>
+
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
